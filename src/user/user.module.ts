012adfca0a38d49cfbcd7c8e56031f80403de648
blessingsmtonga362-cpu <blessingsmtonga362@gmail.com>
2026-06-04 01:17:02 +0200

    user.module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -27,21 +27,3 @@ import { PersonalDetails } from 'src/application/entities/personal_details.entit
   exports: [UserService, JwtModule],
 })
 export class UserModule {}
-/*
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { User } from './entities/user.entity';
-
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    ConfigModule,
-  ],
-  controllers: [UserController],
-  providers: [UserService],
-  exports: [UserService]
-})
-export class UserModule {}*/
