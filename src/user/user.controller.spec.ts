028998069e834662be5f629826c38323ddc2b433
blessingsmtonga362-cpu <blessingsmtonga362@gmail.com>
2026-06-04 02:40:49 +0200

    last test then we should start trying onling database

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { EmailService } from '../email/email.service';
+import { PersonalDetails } from '../application/entities/personal_details.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
@@ -18,10 +21,22 @@ describe('UserController', () => {
           useValue: {},
         },
         {
+          provide: getRepositoryToken(PersonalDetails),
+          useValue: {},
+        },
+        {
           provide: ConfigService,
           useValue: {
             get: jest.fn(),
           },
+        },
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+        {
+          provide: EmailService,
+          useValue: {},
         },
       ],
     }).compile();
